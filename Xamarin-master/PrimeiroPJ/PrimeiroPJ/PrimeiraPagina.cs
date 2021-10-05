f733e36280f59614beb8ecde91893ea5d372a29e
gianpelegrini <giancarlo20100@gmail.com>
2021-10-05 19:31:04 -0300

    projeto concluido

---
+++ Xamarin-master/PrimeiroPJ/PrimeiroPJ/PrimeiraPagina.cs
@@ -0,0 +1,42 @@
+﻿using Java.Sql;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PrimeiroPJ
+{
+    public partial class PrimeiraPagina : ContentPage
+    {
+
+        void btnOk_Clicked(object sender, EventArgs e)
+        {
+            var username = entryUsername.Text;
+            var password = entryPassword.Text;
+
+            if (username=="admin" && password=="@admin")
+            {
+                DisplayAlert("Alerta:", "Login realizado com sucesso!", "OK");
+            }
+            else
+            {
+                DisplayAlert("Alerta:", "Login falhou!", "OK");
+            }
+        }
+
+        void btnLimpar_Clicked(object sender, System.EventArgs e)
+        {
+            entryUsername.Text = "";
+            entryPassword.Text = "";
+
+
+        }
+
+
+        void btnCreditos_Clicked(object sender, System.EventArgs e)
+        {
+            DisplayAlert("Autores:", "Gian", "OK");
+        }
+    }
+}
